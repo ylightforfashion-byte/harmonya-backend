88322097076da4e105974a1b683a031c16fbb97d
ylightforfashion-byte <ylightforfashion@gmail.com>
2026-05-04 14:10:02 +0200

    Refactor Drive file functions and use CONFIG
    Refactor Google Drive file handling functions to use CONFIG for credentials and rename download function.

--- src/drive.js
+++ src/drive.js
@@ -1,36 +1,25 @@
-// drive.js
 import { google } from "googleapis";
-import fs from "fs";
+import { CONFIG } from "./config.js";
 
-const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
-  scopes: ["https://www.googleapis.com/auth/drive.readonly"]
-});
+function getDriveClient() {
+  const auth = new google.auth.GoogleAuth({
+    credentials: JSON.parse(CONFIG.GOOGLE_SERVICE_ACCOUNT),
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+  });
 
-const drive = google.drive({ version: "v3", auth });
+  return google.drive({ version: "v3", auth });
+}
 
-export async function downloadDriveFile(fileId) {
-  try {
-    const response = await drive.files.get(
-      {
-        fileId,
-        alt: "media"
-      },
-      { responseType: "arraybuffer" }
-    );
+export async function getDriveFileStream(fileId) {
+  const drive = getDriveClient();
 
-    const fileMeta = await drive.files.get({
+  const res = await drive.files.get(
+    {
       fileId,
-      fields: "name, mimeType"
-    });
+      alt: "media",
+    },
+    { responseType: "stream" }
+  );
 
-    return {
-      buffer: Buffer.from(response.data),
-      mimeType: fileMeta.data.mimeType,
-      fileName: fileMeta.data.name
-    };
-  } catch (err) {
-    console.error("Error downloading file from Drive:", err);
-    throw new Error("Drive download failed");
-  }
+  return res.data; // Stream du fichier
 }
