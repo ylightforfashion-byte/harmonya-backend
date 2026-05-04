589d3c51bb588b5d2fecfa2cdaed2dcfb584f61c
ylightforfashion-byte <ylightforfashion@gmail.com>
2026-05-04 14:31:00 +0200

    Implement Google Drive file streaming functionality
    Add Google Drive API client and file streaming function

--- drive.js
+++ drive.js
@@ -1,1 +1,25 @@
+import { google } from "googleapis";
+import { CONFIG } from "./config.js";
 
+function getDriveClient() {
+  const auth = new google.auth.GoogleAuth({
+    credentials: JSON.parse(CONFIG.GOOGLE_SERVICE_ACCOUNT),
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+  });
+
+  return google.drive({ version: "v3", auth });
+}
+
+export async function getDriveFileStream(fileId) {
+  const drive = getDriveClient();
+
+  const res = await drive.files.get(
+    {
+      fileId,
+      alt: "media",
+    },
+    { responseType: "stream" }
+  );
+
+  return res.data; // Stream du fichier
+}
