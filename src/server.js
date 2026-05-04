69958b2eaf2f2ea32bfc5db8a13b5be690a00840
ylightforfashion-byte <ylightforfashion@gmail.com>
2026-05-04 11:24:33 +0200

    Remove download route from server
    Removed download route from server.js.

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import { CONFIG } from "./config.js";
 import checkoutRoute from "./checkoutRoute.js";
 import { stripeWebhookHandler, stripeRawBody } from "./stripeWebhook.js";
-import { handleDownload } from "./downloadRoute.js";
 import { successHandler } from "./success.js";
 import { downloadHandler } from "./download.js";
 
@@ -24,9 +23,6 @@ app.use(express.json());
 
 // Checkout route
 app.use("/", checkoutRoute);
-
-// Download route
-app.get("/dl/:token", handleDownload);
 
 app.get("/", (req, res) => {
   res.send("Harmony Lab backend is running.");
